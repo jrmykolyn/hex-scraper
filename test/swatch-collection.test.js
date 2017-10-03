767b10b2daa82979a48787c4941c819195de57eb
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-10-02 21:49:29 -0400

    Stub out tests for 'SwatchCollection' module/class.

---
+++ test/swatch-collection.test.js
@@ -0,0 +1,28 @@
+// --------------------------------------------------
+// IMPORT MODULES
+// --------------------------------------------------
+// Node
+
+// Vendor
+import test from 'ava';
+
+// Project
+import SwatchCollection from '../dist/lib/modules/swatch-collection';
+
+// --------------------------------------------------
+// DECLARE TESTS
+// --------------------------------------------------
+// MODULE TESTS
+test.todo( 'MODULE should be importable.' );
+
+// CLASS TESTS
+test.todo( 'CLASS should expose the `extractColors()` method.' );
+
+test.todo( 'CLASS should be instantiable' );
+
+test.todo( 'CLASS should throw an error when invoked with missing or invalid arguments.' );
+
+// INSTANCE TESTS
+test.todo( 'INSTANCE should expose the `transparent()` method.' );
+
+test.todo( 'INSTANCE should expose the `opaque()` method.' );

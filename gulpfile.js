565ea3797a0e308b61046d3571bb4ad3bd5748bf
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-10-06 08:55:21 -0400

    chore(gulp): Add 'styles' task. Fix 'build' task documentation.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 // --------------------------------------------------
 // IMPORT MODULES
 // --------------------------------------------------
+// Vendor
 var gulp = require( 'gulp' );
+var sass = require( 'gulp-sass' );
 var ts = require( 'gulp-typescript' );
 
 // --------------------------------------------------
@@ -17,10 +19,21 @@ var ts = require( 'gulp-typescript' );
 gulp.task( 'default', [ 'build' ] );
 
 /**
- * Wrapper around any/all style-related tasks.
+ * Wrapper around any/all build-related tasks.
  */
 gulp.task( 'build', function() {
 	gulp.src( "./src/**/*.ts" )
 		.pipe( ts() )
 		.pipe( gulp.dest( './dist' ) );
+} );
+
+/**
+ * Wrapper around any/all style-related tasks.
+ */
+gulp.task( 'styles', function() {
+	gulp.src( `${__dirname}/src/data/*.scss` )
+		.pipe( sass( {
+			outputStyle: 'expanded',
+		} ) )
+		.pipe( gulp.dest( `${__dirname}/src/data` ) );
 } );
